e02bcc2af8cf0c402a30aad733eb3e30834da936
Jimmy Russel Kurniawan <80058599+jimmyrk@users.noreply.github.com>
2023-12-21 20:48:29 +0700

    update

--- assertion.cy.js
+++ assertion.cy.js
@@ -1,12 +1,15 @@
 /// <reference types="cypress" />
 
-// const { it } = require("mocha")
-
 describe('browser action', () => {
     it('should load correct url', () => {
         cy.visit('https://example.com/', { timeout: 10000 })
     });
+
     it('should check correct url', () => {
         cy.url().should('include', 'example.com')
+    });
+
+    it('should check for correct element on the page', () => {
+        cy.get('h1').should('be.visible');
     });
 });
